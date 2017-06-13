b7879f3ea00cab59dd2f93288d4bc08782fbaba1
Byron Ellacott <code@bje.id.au>
2017-06-13 16:26:27 +1000

    ready for trials

--- src/Main.hs
+++ src/Main.hs
@@ -6,14 +6,12 @@ module Main where
 import           Control.Concurrent             (forkIO, newEmptyMVar, tryPutMVar, takeMVar, forkIO, threadDelay)
 import           Control.Concurrent.STM         (TVar, atomically, newTVar, readTVar, modifyTVar')
 import           Control.Exception              (catch, throwIO)
-import           Control.Monad                  (void, forever)
-import           Control.Monad.IO.Class         (MonadIO, liftIO)
-import           Control.Monad.State            (StateT, evalStateT)
-import           Control.Monad.Trans.Unlift     (MonadBaseUnlift, askUnliftBase, unliftBase)
+import           Control.Monad                  (void, forever, when)
 import           Data.Attoparsec.Text           (parse, IResult (..))
 import           Data.Monoid                    ((<>))
 import           Data.Text                      (Text)
 import qualified Data.Text as T
+import           Data.Time.Clock                (getCurrentTime, diffUTCTime)
 import           Reaper
 import           System.Cron
 import           System.IO.Error                (isDoesNotExistError)
@@ -23,11 +21,12 @@ import           System.Posix.Process           (getAnyProcessStatus)
 import           System.Posix.Signals           (Handler (Catch), installHandler, sigCHLD, sigINT, sigTERM, sigKILL,
                                                  signalProcess, setStoppedChildFlag, Signal)
 import           System.Posix.Types             (ProcessID)
+import           System.Process                 (callProcess)
 
 data Configuration = Configuration
     { runAtStart :: Bool
     , schedule   :: CronSchedule
-    , command    :: [Text]
+    , command    :: [String]
     } deriving Show
 
 parseArgs :: [String] -> (Bool, [Text])
@@ -39,7 +38,7 @@ getConfiguration :: Monad m => [String] -> m Configuration
 getConfiguration args = do
     let (ras, expr) = parseArgs args
     (cmd, sched) <- parseLoop expr (parse cronScheduleLoose)
-    return $ Configuration ras sched cmd
+    return $ Configuration ras sched (T.unpack <$> cmd)
   where
     parseLoop []     _ = fail "unexpected end of arguments while reading cron expression"
     parseLoop (t:ts) p = case p (t <> " ") of
@@ -48,48 +47,63 @@ getConfiguration args = do
         Done " " r   -> pure (ts, r)
         Done i _     -> fail $ "unrecognised input at end of cron expression: " <> T.unpack i
 
-runConfiguration :: Configuration -> StateT Int IO (FilePath, [String])
-runConfiguration (Configuration due sched cmd) = undefined
+info :: String -> IO ()
+info msg = do
+    now <- getCurrentTime
+    putStrLn (show now <> ": " <> msg)
+
+runConfiguration :: Configuration -> IO (FilePath, [String])
+runConfiguration conf@(Configuration _ sched cmd) = do
+    now <- getCurrentTime
+    let maxSleep = 2^(20 :: Integer)
+    let next = nextMatch sched now
+    let delay = round . (* 1000000) <$> flip diffUTCTime now <$> next :: Maybe Integer
+    let sleep = maybe maxSleep (max maxSleep) delay
+    threadDelay (fromIntegral sleep)
+    upd <- getCurrentTime
+    if scheduleMatches sched upd
+    then info ("Executing " <> unwords cmd) >> return (head cmd, tail cmd)
+    else runConfiguration conf
+
+tt :: IO ()
+tt = pid1 $ getConfiguration (words "* * * * * /bin/date") >>= runConfiguration
 
 main :: IO ()
 main = do
     conf <- getArgs >>= getConfiguration
-    evalStateT (pid1 $ runConfiguration conf) 0
+    when (runAtStart conf) $ callProcess (head $ command conf) (tail $ command conf)
+    pid1 $ runConfiguration conf
 
 {- pid1 package isn't general purpose enough for this, so below code is largely taken from there  -}
 
-voidIO :: (MonadBaseUnlift IO m, MonadIO m) => IO a -> m ()
-voidIO = liftIO . void
-
 -- Given an action producing commands to be executed, pid1 will reap all children forever
-pid1 :: (MonadBaseUnlift IO m, MonadIO m) => m (FilePath, [String]) -> m ()
+pid1 :: IO (FilePath, [String]) -> IO ()
 pid1 execf = do
-    errno <- liftIO subreaper
+    errno <- subreaper
     if errno /= 0
     then fail $ "error " <> show errno
     else pure ()
 
-    exiting <- liftIO newEmptyMVar
-    children <- liftIO . atomically $ newTVar []
+    exiting <- newEmptyMVar
+    children <- atomically $ newTVar []
 
     -- Catch SIGTERM and SIGINT to exit all processes
     let startExiting = void . tryPutMVar exiting
-    voidIO $ installHandler sigTERM (Catch $ startExiting sigTERM) Nothing
-    voidIO $ installHandler sigINT  (Catch $ startExiting sigINT)  Nothing
+    void $ installHandler sigTERM (Catch $ startExiting sigTERM) Nothing
+    void $ installHandler sigINT  (Catch $ startExiting sigINT)  Nothing
 
     -- Catch SIGCHLD, ignoring stopped children
-    voidIO $ setStoppedChildFlag False
-    voidIO $ installHandler sigCHLD (Catch $ reapWaiting children) Nothing
+    void $ setStoppedChildFlag False
+    void $ installHandler sigCHLD (Catch $ reapWaiting children `ifNotExistsThen` ()) Nothing
 
     -- Ask for the next process to be run
-    u <- askUnliftBase
-    voidIO $ forkIO $ forever $ do
-        proc <- unliftBase u execf
-        print proc
+    void $ forkIO $ forever $ do
+        (fp, args) <- execf
+        callProcess fp args
 
     -- Then just wait forever for the `exiting` flag to be set.  This should happen on the main thread.
-    sig <- liftIO $ takeMVar exiting
-    voidIO $ killAllChildren children sig
+    sig <- takeMVar exiting
+    void $ killAllChildren children sig
 
 ifNotExistsThen :: IO a -> a -> IO a
 ifNotExistsThen io a = io `catch` \e ->
@@ -110,17 +124,23 @@ logErrorExit pids (pid, status) = do
 reapWaiting :: TVar [ProcessID] -> IO ()
 reapWaiting pids = do
     mstatus <- getAnyProcessStatus True False
+    print mstatus
     case mstatus of
         Nothing     -> pure ()
         Just status -> logErrorExit pids status
 
 killAllChildren :: TVar [ProcessID] -> Signal -> IO ()
-killAllChildren pids sig = flip ifNotExistsThen () $ do
+killAllChildren pids sig = do
     -- fork a process to send the TERM then KILL signals to all processes
-    void $ forkIO $ do
+    void $ forkIO $ flip ifNotExistsThen () $ do
+        info "Sending all processes the TERM signal..."
         signalProcess sigTERM (-1)
         threadDelay $ 5 * 1000 * 1000
+        info "Sending all processes the KILL signal..."
         signalProcess sigKILL (-1)
 
     -- reap all children until there are no more, then terminate
-    void $ forever reapWaiting pids `ifNotExistsThen` exitWith (ExitFailure (fromIntegral sig + 128))
+    void $ forever $ reapWaiting pids `catch` \e -> 
+        if isDoesNotExistError e
+        then exitWith (ExitFailure (fromIntegral sig + 128))
+        else throwIO e
