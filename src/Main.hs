694188a8284b94bf543541e1aad99e5365f2ec4b
Byron Ellacott <code@bje.id.au>
2017-06-22 15:15:43 +1000

    rework to catch failed subprocesses properly

--- src/Main.hs
+++ src/Main.hs
@@ -4,7 +4,8 @@
 module Main where
 
 import           Control.Concurrent             (forkIO, newEmptyMVar, tryPutMVar, takeMVar, forkIO, threadDelay)
-import           Control.Concurrent.STM         (TVar, atomically, newTVar, readTVar, modifyTVar')
+import           Control.Concurrent.STM         (atomically, TVar, readTVar, writeTVar, newTVar)
+import           Control.Concurrent.STM.TChan   (TChan, newTChan, readTChan, writeTChan)
 import           Control.Exception              (catch, throwIO)
 import           Control.Monad                  (void, forever, when)
 import           Data.Attoparsec.Text           (parse, IResult (..))
@@ -16,12 +17,12 @@ import           Reaper
 import           System.Cron
 import           System.IO.Error                (isDoesNotExistError)
 import           System.Environment             (getArgs)
-import           System.Exit                    (ExitCode (ExitFailure), exitWith)
-import           System.Posix.Process           (getAnyProcessStatus, getProcessID)
+import           System.Exit                    (ExitCode (..), exitWith)
+import           System.Posix.Process           (getAnyProcessStatus, getProcessID, forkProcess, executeFile,
+                                                 ProcessStatus (..))
 import           System.Posix.Signals           (Handler (Catch), installHandler, sigCHLD, sigINT, sigTERM, sigKILL,
                                                  signalProcess, setStoppedChildFlag, Signal)
 import           System.Posix.Types             (ProcessID)
-import           System.Process                 (callProcess)
 
 data Configuration = Configuration
     { runAtStart :: Bool
@@ -68,17 +69,27 @@ runConfiguration conf@(Configuration _ sched cmd) = do
 tt :: IO ()
 tt = pid1 $ getConfiguration (words "* * * * * /bin/date") >>= runConfiguration
 
-main :: IO ()
-main = do
+-- Launch a sub-process
+execute :: FilePath -> [String] -> IO ProcessID
+execute proc args = forkProcess (executeFile proc True args Nothing)
+
+runCrocker :: IO ()
+runCrocker = do
     pid <- getProcessID
     if pid == 1
     then do
         conf <- getArgs >>= getConfiguration
-        when (runAtStart conf) $ callProcess (head $ command conf) (tail $ command conf)
+        --TODO when (runAtStart conf) $ callProcess (head $ command conf) (tail $ command conf)
         pid1 $ runConfiguration conf
     else putStrLn "This program must be run as PID 1 inside a docker container."
 
 {- pid1 package isn't general purpose enough for this, so below code is largely taken from there  -}
+
+-- Wait for the specified PID to terminate
+waitPid :: ProcessID -> TChan (ProcessID, a) -> IO a
+waitPid pid terminated = do
+    (ded, code) <- atomically $ readTChan terminated
+    if ded == pid then return code else waitPid pid terminated
 
 -- Given an action producing commands to be executed, pid1 will reap all children forever
 pid1 :: IO (FilePath, [String]) -> IO ()
@@ -88,8 +99,10 @@ pid1 execf = do
     then fail $ "error " <> show errno
     else pure ()
 
-    exiting <- newEmptyMVar
-    children <- atomically $ newTVar []
+    -- Shared state
+    exiting    <- newEmptyMVar
+    terminated <- atomically newTChan
+    watching   <- atomically $ newTVar False
 
     -- Catch SIGTERM and SIGINT to exit all processes
     let startExiting = void . tryPutMVar exiting
@@ -98,16 +111,24 @@ pid1 execf = do
 
     -- Catch SIGCHLD, ignoring stopped children
     void $ setStoppedChildFlag False
-    void $ installHandler sigCHLD (Catch $ reapWaiting children `ifNotExistsThen` ()) Nothing
+    void $ installHandler sigCHLD (Catch $ reapWaiting watching terminated `ifNotExistsThen` ()) Nothing
 
     -- Ask for the next process to be run
     void $ forkIO $ forever $ do
         (fp, args) <- execf
-        callProcess fp args
+        atomically $ writeTVar watching True
+        pid <- execute fp args
+        code <- waitPid pid terminated
+        atomically $ writeTVar watching False
+        case code of
+            Exited ExitSuccess     -> info $ "Task completed successfully"
+            Exited (ExitFailure c) -> info $ "Task terminated abnormally with exit code " <> show c
+            Terminated s d         -> info $ "Task terminated by signal " <> show s <> if d then " (core dumped)" else ""
+            Stopped s              -> info $ "Task stopped by signal " <> show s <> " (this should not happen here)"
 
     -- Then just wait forever for the `exiting` flag to be set.  This should happen on the main thread.
     sig <- takeMVar exiting
-    void $ killAllChildren children sig
+    void $ killAllChildren watching terminated sig
 
 ifNotExistsThen :: IO a -> a -> IO a
 ifNotExistsThen io a = io `catch` \e ->
@@ -115,26 +136,21 @@ ifNotExistsThen io a = io `catch` \e ->
     then pure a
     else throwIO e
 
-logErrorExit :: (Eq a, Show a, Show b) => TVar [a] -> (a, b) -> IO ()
-logErrorExit pids (pid, status) = do
-    p <- atomically $ do
-        kids <- readTVar pids
-        if pid `elem` kids
-        then modifyTVar' pids (filter (/= pid)) >> pure (Just (pid, status))
-        else pure Nothing
-    mapM_ print p
-
 -- waitpid() on all terminated child processes; log error exit code if it was a directly spawned child
-reapWaiting :: TVar [ProcessID] -> IO ()
-reapWaiting pids = do
-    mstatus <- getAnyProcessStatus True False
-    print mstatus
+reapWaiting :: TVar Bool -> TChan (ProcessID, ProcessStatus) -> IO ()
+reapWaiting isWatching channel = do
+    mstatus <- getAnyProcessStatus blocking noStopped
     case mstatus of
         Nothing     -> pure ()
-        Just status -> logErrorExit pids status
+        Just status -> atomically $ do
+            watch <- readTVar isWatching
+            when watch $ writeTChan channel status
+  where
+    blocking = True
+    noStopped = False
 
-killAllChildren :: TVar [ProcessID] -> Signal -> IO ()
-killAllChildren pids sig = do
+killAllChildren :: TVar Bool -> TChan (ProcessID, ProcessStatus) -> Signal -> IO ()
+killAllChildren watch chan sig = do
     -- fork a process to send the TERM then KILL signals to all processes
     void $ forkIO $ flip ifNotExistsThen () $ do
         info "Sending all processes the TERM signal..."
@@ -144,7 +160,7 @@ killAllChildren pids sig = do
         signalProcess sigKILL (-1)
 
     -- reap all children until there are no more, then terminate
-    void $ forever $ reapWaiting pids `catch` \e -> 
+    void $ forever $ reapWaiting watch chan `catch` \e -> 
         if isDoesNotExistError e
         then exitWith (ExitFailure (fromIntegral sig + 128))
         else throwIO e
